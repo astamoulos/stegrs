eec768e7f199bb714c720fdef5fd035b07ebb034
astamoulos <alexstamoulos11@gmail.com>
2024-08-25 20:52:38 +0300

    adding clippy for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file"); 
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
