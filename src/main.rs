7fb7d949255de5dfac1551c516e0bb6fbe8ff61a
astamoulos <alexstamoulos11@gmail.com>
2024-09-02 16:48:02 +0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use clap::Parser;
+use std::io::Cursor;
+use image::ImageReader;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
-    pattern: String,
+    image: std::path::PathBuf,
     /// The path to the file to read
-    path: std::path::PathBuf,
+    message: std::path::PathBuf,
+    /// The output image
+    output: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file"); 
+    
+    let img = ImageReader::open(args.image)?.decode()?;
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+    println!("pattern: {:?}, path: {:?}", args.message, args.output)
 }
